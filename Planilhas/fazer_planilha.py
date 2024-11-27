e29c9884f2631ff7fe7906b5350022493de6baae
Davicsb <144847037+Davicsb@users.noreply.github.com>
2024-11-27 18:12:30 -0300

    Add files via upload

--- Planilhas/fazer_planilha.py
+++ Planilhas/fazer_planilha.py
@@ -13,6 +13,10 @@ prazo = np.random.randint(0, 101, num_equipes)
 complexidade = np.random.randint(0, 101, num_equipes)
 demanda = np.random.randint(0, 101, num_equipes)
 dependencia = np.random.randint(0, 101, num_equipes)
+tamanho = np.random.randint(0, 101, num_equipes)
+historico = np.random.randint(0, 101, num_equipes)
+carga = np.random.randint(0, 101, num_equipes)
+escopo = np.random.randint(0, 101, num_equipes)
 
 # Criando o DataFrame
 df = pd.DataFrame({
@@ -22,11 +26,15 @@ df = pd.DataFrame({
     "Prazo (0-100)": prazo,
     "Complexidade do projeto (0-100)": complexidade, 
     "Demanda do mercado (0-100)": demanda,
-    "Dependência da tecnologia de terceiros (0-100)": dependencia 
+    "Dependência da tecnologia de terceiros (0-100)": dependencia,
+    "Tamanho da equipe (0-100)" : tamanho,
+    "Histórico de sucesso da equipe": historico,
+    "Carga horária da equipe (0-100)": carga,
+    "Escopo do projeto (0-100)": escopo
 })
 
 # Salvando a planilha em formato Excel
 file_path = "equipes_projeto.xlsx"
 df.to_excel(file_path, index=False)
-
+print("Dados cadastrados com sucesso!")
 file_path
